35278390959679ca6192e6d0cc56f72816ffa2a4
BAD. <badmodertwitch@gmail.com>
2026-09-07 10:50:59 +0000

    fix(ci): use configured Docker Hub namespace

--- scripts/dockerhub-build-push-images.ps1
+++ scripts/dockerhub-build-push-images.ps1
@@ -1,5 +1,9 @@
 $ErrorActionPreference = "Stop"
 
+if (-not $env:DOCKERHUB_USERNAME) {
+    throw "Set DOCKERHUB_USERNAME to the Docker Hub account namespace"
+}
+
 if (-not $env:IMAGE_TAG) {
     throw "Set IMAGE_TAG to the release tag you want to build and push"
 }
@@ -7,7 +11,7 @@ if (-not $env:IMAGE_TAG) {
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 $env:IMAGE_REGISTRY = "docker.io"
-$env:IMAGE_NAMESPACE = "3252a8"
+$env:IMAGE_NAMESPACE = $env:DOCKERHUB_USERNAME.ToLowerInvariant()
 
 & (Join-Path $scriptDir "docker-build-images.ps1")
 & (Join-Path $scriptDir "docker-push-images.ps1")
